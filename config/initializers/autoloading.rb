348c597d3c899003bce2fc68c4c48f652c9eaa97
woarewe <rostislav.zhuravsky@gmail.com>
2023-04-08 18:20:11 +0200

    Move all the entities to a seaprate dir

--- config/initializers/autoloading.rb
+++ config/initializers/autoloading.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 ActiveSupport::Dependencies.autoload_paths.delete(Rails.root.join("app/types").to_s)
+ActiveSupport::Dependencies.autoload_paths.delete(Rails.root.join("app/entities").to_s)
 
 Rails.autoloaders.main.push_dir(Rails.root.join("app/types"), namespace: Types)
+Rails.autoloaders.main.push_dir(Rails.root.join("app/entities"), namespace: Types)
 
 Rails.autoloaders.each do |autoloader|
   autoloader.inflector.inflect(
