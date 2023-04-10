dc77b633e0d65ff185b6e0183ef4674622406ac1
woarewe <rostislav.zhuravsky@gmail.com>
2023-04-10 06:14:00 +0200

    Final fixes

--- app/gateways/rest/api/talents/index.rb
+++ app/gateways/rest/api/talents/index.rb
@@ -6,8 +6,8 @@ module REST
       class Index < Base
         desc "Show talents"
         get do
-          pagination_params!(params)
-            .then { |validated| present_paginated(::Talent.all, **validated, with: Serialization::Talent) }
+          validated = pagination_params!(params)
+          present_paginated(::Repositories::Talent.all, **validated, with: Serialization::Talent)
         end
       end
     end
